61e8203e24e0a53bb70bc84b00e2a544526273cb
tjrwls9881 <115977303+tjrwls9881@users.noreply.github.com>
2023-04-04 22:18:50 +0900

    Add files via upload

---
+++ forty/src/main/java/com/billions/forty/FortyApplication.java
@@ -0,0 +1,13 @@
+package com.billions.forty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortyApplication.class, args);
+	}
+
+}
