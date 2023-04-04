674bdfa959f7b0f8b909014f3d16970f685ef5c1
tjrwls9881 <115977303+tjrwls9881@users.noreply.github.com>
2023-04-04 22:18:50 +0900

    feat:user CRUD

---
+++ forty/src/main/java/com/billions/forty/user/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.billions.forty.user.mapper;
+
+import com.billions.forty.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
---
+++ forty/src/test/java/com/billions/forty/FortyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.billions.forty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
